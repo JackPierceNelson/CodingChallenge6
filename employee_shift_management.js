89f50b3a3296f9671048d2b7465ab7381312f093
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-09-21 11:06:15 -0500

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -5,3 +5,12 @@ const employees = [
     {name: "David", shifts:[{day: "Monday", hours: 8}]},
     {name: "Emily", shifts:[{day: "Friday", hours: 8}]}
 ];
+
+// Task 2: Create a Function to Display Employee Shift Details
+
+function displayEmployeeShifts(employee) {
+    console.log(`Employee: ${employee.name}`);
+    console.log("Shifts");
+    employee.shifts.forEach(shift => {console.log(`${shift.day}:${shift.hours} hours`);});
+}
+employees.forEach(displayEmployeeShifts);
